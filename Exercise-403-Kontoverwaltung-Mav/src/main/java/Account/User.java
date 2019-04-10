5c400f89f902aff02beec77ba8defdb13b811230
goealb16 <goedl.alex10@gmail.com>
2019-04-10 12:50:24 +0200

    Perform Test

---
+++ Exercise-403-Kontoverwaltung-Mav/src/main/java/Account/User.java
@@ -0,0 +1,18 @@
+/*
+#ProProgrammer
+*/
+package Account;
+
+
+public class User {
+    private String name;
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+    
+}
